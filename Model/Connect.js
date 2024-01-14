e69c4574f2b55807675f92b6db2870722dfd0614
Shailesh Kaliya <117556933+shaileshkaliya@users.noreply.github.com>
2024-01-14 16:54:15 +0530

    Update Connect.js

--- Model/Connect.js
+++ Model/Connect.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 
-// const uri = 'mongodb+srv://shaileshkaliya124:yiBL8NoMmnmPZBdz@ecommerce.aoucz3f.mongodb.net/Ecommerce?retryWrites=true&w=majority'
 const connectDB = (uri) => {
     console.log("Connected Database Successfully ! ");
     return mongoose.connect(uri);
